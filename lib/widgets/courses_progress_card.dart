58e0bde5fa6048d0185cc0f702748849a9705412
hirodori <fernandokondo@alunos.utfpr.edu.br>
2023-05-27 23:07:34 -0300

    color amber to pink

--- lib/widgets/courses_progress_card.dart
+++ lib/widgets/courses_progress_card.dart
@@ -6,7 +6,7 @@ class ProgressCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.amber,
+      color: Colors.pink,
     );
   }
 }
