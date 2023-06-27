b0bdb0b2110c4c594ba18e3e4907085ecc0f58d7
hirodori <fernandokondo@alunos.utfpr.edu.br>
2023-06-27 18:29:07 -0300

    última modif Login

--- lib/modules/user_preferences.dart
+++ lib/modules/user_preferences.dart
@@ -1,32 +1,42 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:courseland/modules/user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserPreferences {
   static late SharedPreferences _preferences;
 
-  static const _keyUser = 'userl';
-  static const myUser = Userl(
+  static const _keyUser = 'user';
+  static const myUser = LocalUser(
+    //id: 'aaa',
     imagePath:
         'https://i.pinimg.com/564x/c5/9e/42/c59e4220f5710c277cfd9d0f137b13f5.jpg',
-    name: 'Fernando Kondo',
-    email: 'fernandokondo@alunos.utfpr.edu.br',
+    name: 'Random Aleatório',
+    email: 'random@gmail.com',
     isDarkMode: false,
   );
 
   static Future init() async =>
       _preferences = await SharedPreferences.getInstance();
 
-  static Future setUser(Userl userl) async {
-    final json = jsonEncode(userl.toJson());
+  static Future setUser(LocalUser user) async {
+    final json = jsonEncode(user.toJson());
 
     await _preferences.setString(_keyUser, json);
   }
 
-  static UserlgetUser() {
+  static getUser() {
     final json = _preferences.getString(_keyUser);
 
-    return json == null ? myUser : Userl.fromJson(jsonDecode(json));
+    return json == null ? myUser : LocalUser.fromJson(jsonDecode(json));
   }
+
+/*
+  Stream<List<LocalUser>> readUsers() => FirebaseFirestore.instance
+      .collection('users')
+      .snapshots()
+      .map((snapshot) =>
+          snapshot.docs.map((doc) => LocalUser.fromJson(doc.data())).toList());
+*/
 }
